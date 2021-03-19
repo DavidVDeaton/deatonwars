0021d53b5d304f90b8a488ad7462eb1f6514c83e
David Deaton <dideater@gmail.com>
2021-03-19 09:03:17 -0700

    initial commit

---
+++ schedule.js
@@ -0,0 +1,177 @@
+$("#team1").text("10 Virginia Tech");
+$("#team2").text("7 Florida");
+$("#team3").text("14 Colgate");
+$("#team4").text("3 Arkansas");
+$("#team5").text("16 Drexel");
+$("#team6").text("1 Illinois");
+$("#team7").text("11 Utah State");
+$("#team8").text("6 Texas Tech");
+
+$("#team9").text("15 Oral Roberts");
+$("#team10").text("2 Ohio State");
+$("#team11").text("16 Hartford");
+$("#team12").text("1 Baylor");
+$("#team13").text("9 Georgia Tech");
+$("#team14").text("8 Loyola Chicago");
+$("#team15").text("12 Oregon State");
+$("#team16").text("5 Tennessee");
+
+$("#team17").text("13 Liberty");
+$("#team18").text("4 Oklahoma State");
+$("#team19").text("9 Wisconsin");
+$("#team20").text("8 North Caroliina");
+$("#team21").text("15 Cleveland State");
+$("#team22").text("2 Houston");
+$("#team23").text("13 North Texas");
+$("#team24").text("4 Purdue");
+
+$("#team25").text("10 Rutgers");
+$("#team26").text("7 Clemson");
+$("#team27").text("11 Syracuse");
+$("#team28").text("6 San Diego State");
+$("#team29").text("14 Morehead State");
+$("#team30").text("3 West Virginia");
+$("#team31").text("12 Winthrop");
+$("#team32").text("5 Villanova");
+
+$("#team33").text("12 Georgetown");
+$("#team34").text("5 Colorado");
+$("#team35").text("13 UNC Greensboro");
+$("#team36").text("4 Florida State");
+$("#team37").text("14 Eastern Washington");
+$("#team38").text("3 Kansas");
+$("#team39").text("9 St. Bonaventure");
+$("#team40").text("8 LSU");
+
+$("#team41").text("16 Texas Southern");
+$("#team42").text("1 Michigan");
+$("#team43").text("12 UC Santa Barbara");
+$("#team44").text("5 Creighton");
+$("#team45").text("15 Iona");
+$("#team46").text("2 Alabama");
+$("#team47").text("11 Drake");
+$("#team48").text("6 USC");
+
+$("#team49").text("15 Grand Canyon");
+$("#team50").text("2 Iowa");
+$("#team51").text("10 Maryland");
+$("#team52").text("7 U Conn");
+$("#team53").text("13 Ohio");
+$("#team54").text("4 Virginia");
+$("#team55").text("9 Missouri");
+$("#team56").text("8 Oklahoma");
+
+$("#team57").text("16 Norfolk State");
+$("#team58").text("1 Gonzaga");
+$("#team59").text("11 UCLA");
+$("#team60").text("6 BYU");
+$("#team61").text("14 Abilene Christian");
+$("#team62").text("3 Texas");
+$("#team63").text("10 VCU");
+$("#team64").text("7 Oregon");
+
+var david = $("<img>", {
+    class: "small-circle",
+    alt: "David",
+    src: "assets/profile/david.jpg"
+});
+
+var dave = $("<img>", {
+    class: "small-circle",
+    alt: "Dave",
+    src: "assets/profile/dave.jpg"
+});
+
+var april = $("<img>", {
+    class: "small-circle",
+    alt: "April",
+    src: "assets/profile/april.jpg"
+});
+
+var caleb = $("<img>", {
+    class: "small-circle",
+    alt: "Caleb",
+    src: "assets/profile/caleb.jpg"
+});
+
+var ethan = $("<img>", {
+    class: "small-circle",
+    alt: "Ethan",
+    src: "assets/profile/ethan.png"
+});
+
+var katie = $("<img>", {
+    class: "small-circle",
+    alt: "Katie",
+    src: "assets/profile/katie.jpg"
+});
+
+var matt = $("<img>", {
+    class: "small-circle",
+    alt: "Katie",
+    src: "assets/profile/matt.jpg"
+});
+
+var paige = $("<img>", {
+    class: "small-circle",
+    alt: "Paige",
+    src: "assets/profile/paige.jpg"
+});
+
+var noah = $("<img>", {
+    class: "small-circle",
+    alt: "Noah",
+    src: "assets/profile/noah.jpg"
+});
+
+var reed = $("<img>", {
+    class: "small-circle",
+    alt: "Reed",
+    src: "assets/profile/reed.jpg"
+});
+
+var dennis = $("<img>", {
+    class: "small-circle",
+    alt: "Dennis",
+    src: "assets/profile/dennis.jpg"
+});
+
+var susan = $("<img>", {
+    class: "small-circle",
+    alt: "Susan",
+    src: "assets/profile/susan.png"
+});
+
+var hyrum = $("<img>", {
+    class: "small-circle",
+    alt: "Hyrum",
+    src: "assets/profile/hyrum.jpg"
+});
+
+var levi = $("<img>", {
+    class: "small-circle",
+    alt: "Levi",
+    src: "assets/profile/levi.png"
+});
+
+var rachel = $("<img>", {
+    class: "small-circle",
+    alt: "Rachel",
+    src: "assets/profile/rachel.jpg"
+});
+
+$(".david").append(david);
+// $(".dave").append(dave);
+// $(".april").append(april);
+// $(".caleb").append(caleb);
+// $(".ethan").append(ethan);
+$(".katie").append(katie);
+$(".matt").append(matt);
+$(".paige").append(paige);
+$(".noah").append(noah);
+$(".reed").append(reed);
+$(".dennis").append(dennis);
+$(".susan").append(susan);
+$(".hyrum").append(hyrum);
+$(".rachel").append(rachel);
+$(".levi").append(levi);
---
+++ scores.js
@@ -0,0 +1,187 @@
+var david = $("<img>", {
+    class: "med-circle",
+    alt: "David",
+    src: "assets/profile/david.jpg"
+});
+
+var dave = $("<img>", {
+    class: "med-circle",
+    alt: "Dave",
+    src: "assets/profile/dave.jpg"
+});
+
+var april = $("<img>", {
+    class: "med-circle",
+    alt: "April",
+    src: "assets/profile/april.jpg"
+});
+
+var caleb = $("<img>", {
+    class: "med-circle",
+    alt: "Caleb",
+    src: "assets/profile/caleb.jpg"
+});
+
+var ethan = $("<img>", {
+    class: "med-circle",
+    alt: "Ethan",
+    src: "assets/profile/ethan.png"
+});
+
+var katie = $("<img>", {
+    class: "med-circle",
+    alt: "Katie",
+    src: "assets/profile/katie.jpg"
+});
+
+var matt = $("<img>", {
+    class: "med-circle",
+    alt: "Katie",
+    src: "assets/profile/matt.jpg"
+});
+
+var paige = $("<img>", {
+    class: "med-circle",
+    alt: "Paige",
+    src: "assets/profile/paige.jpg"
+});
+
+var noah = $("<img>", {
+    class: "med-circle",
+    alt: "Noah",
+    src: "assets/profile/noah.jpg"
+});
+
+var reed = $("<img>", {
+    class: "med-circle",
+    alt: "Reed",
+    src: "assets/profile/reed.jpg"
+});
+
+var dennis = $("<img>", {
+    class: "med-circle",
+    alt: "Dennis",
+    src: "assets/profile/dennis.jpg"
+});
+
+var susan = $("<img>", {
+    class: "med-circle",
+    alt: "Susan",
+    src: "assets/profile/susan.png"
+});
+
+var hyrum = $("<img>", {
+    class: "med-circle",
+    alt: "Hyrum",
+    src: "assets/profile/hyrum.jpg"
+});
+
+var levi = $("<img>", {
+    class: "med-circle",
+    alt: "Levi",
+    src: "assets/profile/levi.png"
+});
+
+var rachel = $("<img>", {
+    class: "med-circle",
+    alt: "Rachel",
+    src: "assets/profile/rachel.jpg"
+});
+
+var shea = $("<img>", {
+    class: "med-circle",
+    alt: "Shea",
+    src: "assets/profile/shea.png"
+});
+
+var suzy = $("<img>", {
+    class: "med-circle",
+    alt: "Suzy",
+    src: "assets/profile/suzy.jpg"
+});
+
+$(".david").append(david);
+$(".dave").append(dave);
+$(".april").append(april);
+$(".caleb").append(caleb);
+$(".ethan").append(ethan);
+$(".katie").append(katie);
+$(".matt").append(matt);
+$(".paige").append(paige);
+$(".noah").append(noah);
+$(".reed").append(reed);
+$(".dennis").append(dennis);
+$(".susan").append(susan);
+$(".hyrum").append(hyrum);
+$(".rachel").append(rachel);
+$(".levi").append(levi);
+$(".suzy").append(suzy);
+$(".shea").append(shea);
+
+var participants = 17;
+
+var fla1 = participants/8
+var vt1 = participants/3
+var ark1 = participants/15
+var ill1 = participants/15
+var tt1 = participants/4
+var utst1 = participants/7
+var oral1 = participants/1
+var ohst1 = participants/14
+var bay1 = participants/15
+var gt1 = participants/7
+var lc1 = participants/4
+var orst1 = participants/4
+var tenn1 = participants/8
+var lib1 = participants/3
+var okst1 = participants/15
+var wisc1 = participants/15
+var unc1 = participants/4
+var clev1 = participants/7
+var hou1 = participants/1
+var ntex1 = participants/14
+var pur1 = participants/15
+var rut1 = participants/7
+var clem1 = participants/4
+var syra1 = participants/4
+var sdst1 = participants/8
+var mst1 = participants/3
+var wvir1 = participants/15
+var win1 = participants/15
+var vill1 = participants/4
+
+var davidScore = 0;
+var daveScore = 0;
+var aprilScore = 0;
+var calebScore = 0;
+var ethanScore = 0;
+var rachelScore = 0;
+var hyrumScore = 0;
+var leviScore = 0;
+var paigeScore = 0;
+var mattScore = 0;
+var reedScore = 0;
+var katieScore = 0;
+var noahScore = 0;
+var dennisScore = 0;
+var susanScore = 0;
+var suzyScore = 0;
+var sheaScore = 0;
+
+$("#davidScore").text(davidScore);
+$("#dennisScore").text(dennisScore);
+$("#susanScore").text(susanScore);
+$("#daveScore").text(daveScore);
+$("#aprilScore").text(aprilScore);
+$("#calebScore").text(calebScore);
+$("#ethanScore").text(ethanScore);
+$("#leviScore").text(leviScore);
+$("#hyrumScore").text(hyrumScore);
+$("#rachelScore").text(rachelScore);
+$("#paigeScore").text(paigeScore);
+$("#mattScore").text(mattScore);
+$("#katieScore").text(katieScore);
+$("#reedScore").text(reedScore);
+$("#noahScore").text(noahScore);
+$("#suzyScore").text(suzyScore);
+$("#sheaScore").text(sheaScore);
