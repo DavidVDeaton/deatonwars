5134dbd1ef700b53b4110ffbd37cc932e5c437c7
David Deaton <dideater@gmail.com>
2021-03-22 10:33:09 -0700

    sweet 16 added

---
+++ schedule2.js
@@ -0,0 +1,137 @@
+$("#team1").text("1 Baylor");
+$("#team2").text("5 Villanova");
+$("#team3").text("15 Oral Roberts");
+$("#team4").text("3 Arkansas");
+$("#team5").text("12 Oregon State");
+$("#team6").text("8 Loyola Chicago");
+$("#team7").text("11 Syracuse");
+$("#team8").text("2 Houston");
+
+// $("#team9").text("15 Oral Roberts");
+// $("#team10").text("2 Ohio State");
+// $("#team11").text("16 Hartford");
+// $("#team12").text("1 Baylor");
+// $("#team13").text("9 Georgia Tech");
+// $("#team14").text("8 Loyola Chicago");
+// $("#team15").text("12 Oregon State");
+// $("#team16").text("5 Tennessee");
+
+var david = $("<img>", {
+    class: "small-circle",
+    alt: "David",
+    src: "assets/profile/david.jpg"
+});
+
+var dave = $("<img>", {
+    class: "small-circle",
+    alt: "Dave",
+    src: "assets/profile/dave.jpg"
+});
+
+var april = $("<img>", {
+    class: "small-circle",
+    alt: "April",
+    src: "assets/profile/april.jpg"
+});
+
+var caleb = $("<img>", {
+    class: "small-circle",
+    alt: "Caleb",
+    src: "assets/profile/caleb.jpg"
+});
+
+var ethan = $("<img>", {
+    class: "small-circle",
+    alt: "Ethan",
+    src: "assets/profile/ethan.png"
+});
+
+var katie = $("<img>", {
+    class: "small-circle",
+    alt: "Katie",
+    src: "assets/profile/katie.jpg"
+});
+
+var matt = $("<img>", {
+    class: "small-circle",
+    alt: "Katie",
+    src: "assets/profile/matt.jpg"
+});
+
+var paige = $("<img>", {
+    class: "small-circle",
+    alt: "Paige",
+    src: "assets/profile/paige.jpg"
+});
+
+var noah = $("<img>", {
+    class: "small-circle",
+    alt: "Noah",
+    src: "assets/profile/noah.jpg"
+});
+
+var reed = $("<img>", {
+    class: "small-circle",
+    alt: "Reed",
+    src: "assets/profile/reed.jpg"
+});
+
+var dennis = $("<img>", {
+    class: "small-circle",
+    alt: "Dennis",
+    src: "assets/profile/dennis.jpg"
+});
+
+var susan = $("<img>", {
+    class: "small-circle",
+    alt: "Susan",
+    src: "assets/profile/susan.png"
+});
+
+var hyrum = $("<img>", {
+    class: "small-circle",
+    alt: "Hyrum",
+    src: "assets/profile/hyrum2.jpg"
+});
+
+var levi = $("<img>", {
+    class: "small-circle",
+    alt: "Levi",
+    src: "assets/profile/levi2.jpg"
+});
+
+var rachel = $("<img>", {
+    class: "small-circle",
+    alt: "Rachel",
+    src: "assets/profile/rachel.jpg"
+});
+
+var shea = $("<img>", {
+    class: "small-circle",
+    alt: "Shea",
+    src: "assets/profile/shea.png"
+});
+
+var suzy = $("<img>", {
+    class: "small-circle",
+    alt: "Suzy",
+    src: "assets/profile/suzy.jpg"
+});
+
+$(".david").append(david);
+$(".dave").append(dave);
+$(".april").append(april);
+$(".caleb").append(caleb);
+$(".ethan").append(ethan);
+$(".katie").append(katie);
+$(".matt").append(matt);
+$(".paige").append(paige);
+$(".noah").append(noah);
+$(".reed").append(reed);
+$(".dennis").append(dennis);
+$(".susan").append(susan);
+$(".hyrum").append(hyrum);
+$(".rachel").append(rachel);
+$(".levi").append(levi);
+$(".shea").append(shea);
+$(".suzy").append(suzy);
